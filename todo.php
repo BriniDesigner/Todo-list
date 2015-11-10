12d034cd3b24d5ae557e79709d3842850c237351
Brini <brini.abderrahman@hotmail.fr>
2015-11-10 18:05:27 +0100

    Add comments

--- todo.php
+++ todo.php
@@ -6,12 +6,16 @@ define ( 'DB_USER', 'root' );
 define ( 'DB_PASSWORD', '' );
 define ( 'DB_DB', 'todo' );
 
+
+// The Note Class
 class Note {
 
 	private static $conn;
 
 	public static function query($sql){
+		// Connect to the database
 		self::$conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DB);
+		// Execute query
 		$res = self::$conn->query($sql);
 		return $res;
 	}
@@ -32,12 +36,15 @@ class Note {
 		$message = $note['message'];
 
 		if (empty($note['id'])) {
+			// Add a new note
 			$res = self::query("INSERT INTO `notes` (`id`, `title`, `message`) VALUES (NULL, '{$title}', '{$message}')");
 			$res = self::$conn->insert_id;
 		} else {
+			// Update an existing note
 			$id = $note['id'];
 			$res = self::query("UPDATE `notes` SET `title` = '{$title}', `message` = '{$message}' WHERE `notes`.`id` = {$id}");
 		}
+		// Return query result
 		return json_encode($res);
 	}
 
@@ -48,9 +55,12 @@ class Note {
 
 }
 
+// Our script requires JSON data format
 // Set Content-Type to JSON
 header('Content-Type: application/json');
 
+// if GET request then return the list of all items
+// if POST request then add, edit or delete
 switch ($_SERVER['REQUEST_METHOD']) {
 	case 'GET':
 		echo Note::getAll();
@@ -65,7 +75,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
 		break;
 
 	default:
-		die('Denied access');
+		echo 'Unknown operation';
 		break;
 }
 ?>
